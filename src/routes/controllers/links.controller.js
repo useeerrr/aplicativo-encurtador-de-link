4dfbc6f7fa68ff0ed33abba60db29e713af51146
useeerrr <joaolucaspereiralopes34@gmail.com>
2026-08-26 22:37:37 -0300

    modificaçãoDs2 2

--- src/routes/controllers/links.controller.js
+++ src/routes/controllers/links.controller.js
@@ -18,9 +18,20 @@ function linksController() {
 
                 reponse.status(201).json(reponseBody);
             } catch (error) {
-              next(error)
+              next(error);
             }
+        },
 
+        async redirect(request, response, next) {
+            try {
+                const code = request.params.code;
+                const link = await linksService.resolve()
+
+                response.set("Cache-Control", "public, max-age=300")
+                response.redirect(302, link.originalUrl);
+            } catch (error) {
+                next(error);
+            }
         }
     }
 }
